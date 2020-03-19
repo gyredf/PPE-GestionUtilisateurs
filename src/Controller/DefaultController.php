8accf2daffb24507ed628db0efedf9cd4abdefa7
TotoLec <thlecacheux@gmail.com>
2020-03-19 17:32:48 +0100

    [PDF] Try to create PDF

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Dompdf\Dompdf;
 
 class DefaultController extends AbstractController
 {
@@ -16,4 +17,37 @@ class DefaultController extends AbstractController
             'controller_name' => 'DefaultController',
         ]);
     }
+
+	/**
+     * @Route("/pdftest", name="default_pdf")
+     */
+    public function pdf(){
+	    // Instantiate Dompdf with our options
+	    $dompdf = new Dompdf();
+
+	    // Retrieve the HTML generated in our twig file
+	    // $html = file_get_contents('eleves/index.html.twig');
+
+	    // Load HTML to Dompdf
+	    $dompdf->loadHtml('helloWorld');
+
+	    // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+	    $dompdf->setPaper('A4', 'portrait');
+
+	    // Render the HTML as PDF
+	    $dompdf->render();
+
+	    $resolv = $dompdf->stream("mypdf.pdf", [
+        	"Attachment" => false
+	    ]);
+
+
+	    // return $this->render('default/index.html.twig', [
+     //        'controller_name' => 'DefaultController',
+     //    ]);
+
+
+
+	}
+
 }
